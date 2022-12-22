59185c4a4bc5ef647a2e4f1824c0e44bed369003
matt530bailey <115194527+matt530bailey@users.noreply.github.com>
2022-12-22 10:36:06 -0800

    first push

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+// init();
+
+// const fs = require("fs")
+
+function taco(){
+    // fs.writeFile("hello.html", {data: "something"}, (err, data) =>{
+    //     if(err) throw err
+    // })
+    return 5 * 3
+}
+
+console.log(taco())
